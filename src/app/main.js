4b2ccf24aad977e1e979613a2f14ab6779859e25
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2020-09-01 18:44:38 -0500

    Release: Tue Sep  1 18:44:38 CDT 2020

--- src/app/main.js
+++
@@ -1,5 +0,0 @@
-import App from './components/App.svelte';
-
-new App({ // eslint-disable-line
-  target: document.querySelector('#app'),
-});
--- src/app/shared/stores.js
+++
@@ -1,9 +0,0 @@
-import { writable } from 'svelte/store';
-
-export const session = writable({
-  loggedIn: false,
-});
-
-export default {
-  session,
-};
