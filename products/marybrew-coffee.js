bada369259371b76097d774358f44ee03ecffdb8
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2020-09-02 09:48:56 -0500

    Cleanup

--- products/marybrew-coffee.js
+++
@@ -1,9 +0,0 @@
-{
-  "name": "MaryBrew coffee",
-  "label": "MAR NEGRO CAFÉ X SEWARI",
-  "profile": "Oaxaca sierra mixe",
-  "quantity": "3360 msnm",
-  "description": "Proceso despulpado productores familia Martínez.<br />\n«notas de vainilla, cacao semi amargos, acidez tántrica, caramelos, maple»\n",
-  "our_suggestion": "<b>¿Por que tomar café con cbd?</b>\nLa cafeína y el cannabidoid (cbd) trabajan en sinergia, aumentando tu energía y\nconcentración contrarrestando los efectos no deseados de la cafeína como la ansiedad,\ntaquicardia y temblores.\n",
-  "additional_info": "Infusionado con cbd puro. 0% thc / 99.59% cbd"
-}
